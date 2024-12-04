5f456a552e7e16103f691eb85490d1a894962a64
ZzzXCcoder <makaka1337@list.ru>
2024-12-04 16:06:38 +0300

    only_navigation-drawer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.scss"
+
+
+const items = document.querySelectorAll('.navigation-drawer__item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(i => i.classList.remove('navigation-drawer__item--active'));
+    
+
+    item.classList.add('navigation-drawer__item--selected');
+
+
+    items.forEach(i => i.classList.remove('navigation-drawer__item--active'));
+    item.classList.add('navigation-drawer__item--active');
+  });
+});
